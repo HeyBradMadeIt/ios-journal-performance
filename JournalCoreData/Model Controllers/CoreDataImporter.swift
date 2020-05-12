155c68c2acbcf9fd096a053ad7f631d6e9672098
Brad <braddiroff@icloud.com>
2020-05-12 17:19:11 -0400

    Trying to change the fetching process, in progress

--- JournalCoreData/Model Controllers/CoreDataImporter.swift
+++ JournalCoreData/Model Controllers/CoreDataImporter.swift
@@ -9,6 +9,32 @@
 import Foundation
 import CoreData
 
+//import CoreFoundation
+
+class WatchTimer {
+
+    let startTime:CFAbsoluteTime
+    var endTime:CFAbsoluteTime?
+
+    init() {
+        startTime = CFAbsoluteTimeGetCurrent()
+    }
+
+    func stop() -> CFAbsoluteTime {
+        endTime = CFAbsoluteTimeGetCurrent()
+
+        return duration!
+    }
+
+    var duration:CFAbsoluteTime? {
+        if let endTime = endTime {
+            return endTime - startTime
+        } else {
+            return nil
+        }
+    }
+}
+
 class CoreDataImporter {
     init(context: NSManagedObjectContext) {
         self.context = context
@@ -16,7 +42,17 @@ class CoreDataImporter {
     
     func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
         
+        let timer = WatchTimer()
+        
         self.context.perform {
+            
+            let identifiers = entries.compactMap { $0.identifier }
+            let array = self.entriesFromPersistentStore(with: identifiers, in: self.context)
+            
+            for entryRep in entries {
+                
+            }
+            
             for entryRep in entries {
                 guard let identifier = entryRep.identifier else { continue }
                 
@@ -27,16 +63,68 @@ class CoreDataImporter {
                     _ = Entry(entryRepresentation: entryRep, context: self.context)
                 }
             }
+            print("The task took \(timer.stop()) seconds.")
             completion(nil)
         }
     }
     
+    /*
+    func sync(entries: [EntryRepresentation], completion: @escaping (Error?) -> Void = { _ in }) {
+        
+        let timer = WatchTimer()
+        
+        self.context.perform {
+            
+            let identifiers = entries.compactMap { $0.identifier }
+            let array = self.entriesFromPersistentStore(with: identifiers, in: self.context)
+            
+            for entryRep in entries {
+                
+            }
+            
+            for entryRep in entries {
+                guard let identifier = entryRep.identifier else { continue }
+                
+                let entry = self.fetchSingleEntryFromPersistentStore(with: identifier, in: self.context)
+                if let entry = entry, entry != entryRep {
+                    self.update(entry: entry, with: entryRep)
+                } else if entry == nil {
+                    _ = Entry(entryRepresentation: entryRep, context: self.context)
+                }
+            }
+            print("The task took \(timer.stop()) seconds.")
+            completion(nil)
+        }
+    }
+ */
+ 
+    /*
+    private func fetchAll() -> [Entry]{
+        
+    }
+    */
     private func update(entry: Entry, with entryRep: EntryRepresentation) {
         entry.title = entryRep.title
         entry.bodyText = entryRep.bodyText
         entry.mood = entryRep.mood
         entry.timestamp = entryRep.timestamp
         entry.identifier = entryRep.identifier
+    }
+    
+    private func entriesFromPersistentStore(with identifiers: [String], in context: NSManagedObjectContext) -> [Entry] {
+        
+   //     guard let identifiers = identifiers else { return nil }
+        
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiers)
+        
+        var result: [Entry] = []
+        do {
+            result = try context.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching single entry: \(error)")
+        }
+        return result
     }
     
     private func fetchSingleEntryFromPersistentStore(with identifier: String?, in context: NSManagedObjectContext) -> Entry? {
